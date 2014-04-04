655e52389492b83337dad480aecef8822fc17eea
Sebastian Velez <sebasv89@gmail.com>
2014-04-04 12:39:52 -0500

    Adding test example

---
+++ src/test/java/co/edu/eafit/safebuy/util/ResponseMessageTest.java
@@ -0,0 +1,12 @@
+package co.edu.eafit.safebuy.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ResponseMessageTest {
+
+	@Test
+	public void testExample() {
+		Assert.assertEquals(true, true);
+	}
+}
